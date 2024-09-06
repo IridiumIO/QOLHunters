ff2683029f4e237422931b0d2b99e327d90247bb
IridiumIO <iridium.io@outlook.com>
2024-09-06 22:20:21 +1000

    Add individual config toggles for each extension

--- src/main/java/io/iridium/qolhunters/QOLHuntersMixinPlugin.java
+++ src/main/java/io/iridium/qolhunters/QOLHuntersMixinPlugin.java
@@ -15,27 +15,67 @@ import java.util.Set;
 import java.util.function.Supplier;
 
 public final class QOLHuntersMixinPlugin implements IMixinConfigPlugin {
-    private static final Supplier<Boolean> TRUE = () -> true;
 
-    private static final Map<String, Supplier<Boolean>> CONDITIONS = ImmutableMap.of(
-            "io.iridium.qolhunters.mixin.vaultenchanter.MixinEnchantmentCost", () -> !QOLHuntersMixinPlugin.vanillaSafeMode,
-            "io.iridium.qolhunters.mixin.vaultenchanter.MixinVaultEnchanterBlock", () -> !QOLHuntersMixinPlugin.vanillaSafeMode,
-            "io.iridium.qolhunters.mixin.vaultenchanter.MixinVaultEnchanterContainer", () -> !QOLHuntersMixinPlugin.vanillaSafeMode,
-            "io.iridium.qolhunters.mixin.vaultenchanter.MixinVaultEnchanterScreen", () -> !QOLHuntersMixinPlugin.vanillaSafeMode,
-            "io.iridium.qolhunters.mixin.vaultenchanter.MixinVaultEnchanterTileEntity", () -> !QOLHuntersMixinPlugin.vanillaSafeMode
+    private static boolean vanillaSafeMode = false;
+    private static boolean betterDescriptions = true;
+    private static boolean vaultModifierOverlays = true;
+    private static boolean vaultEnchanterEmeraldSlot = true;
+
+
+
+    private static final Supplier<Boolean> TRUE = () -> true;
+    private static final Supplier<Boolean> FALSE = () -> false;
+
+    private static final Map<String, Supplier<Boolean>> BETTER_DESCRIPTIONS_CONDITIONS = ImmutableMap.of(
+            "io.iridium.qolhunters.mixin.configs.MixinAbilitiesDescriptionsConfig", () -> QOLHuntersMixinPlugin.betterDescriptions,
+            "io.iridium.qolhunters.mixin.configs.MixinMenuPlayerStatDescriptionConfig", () -> QOLHuntersMixinPlugin.betterDescriptions,
+            "io.iridium.qolhunters.mixin.configs.MixinSkillDescriptionsConfig", () -> QOLHuntersMixinPlugin.betterDescriptions
+    );
+
+    private static final Map<String, Supplier<Boolean>> VAULT_MODIFIER_OVERLAYS_CONDITIONS = ImmutableMap.of(
+            "io.iridium.qolhunters.mixin.configs.MixinVaultModifiersConfig", () -> QOLHuntersMixinPlugin.vaultModifierOverlays
+    );
+
+    private static final Map<String, Supplier<Boolean>> VAULT_ENCHANTER_EMERALD_SLOT_CONDITIONS = ImmutableMap.of(
+            "io.iridium.qolhunters.mixin.vaultenchanter.MixinEnchantmentCost", () -> QOLHuntersMixinPlugin.vaultEnchanterEmeraldSlot && !QOLHuntersMixinPlugin.vanillaSafeMode,
+            "io.iridium.qolhunters.mixin.vaultenchanter.MixinVaultEnchanterBlock", () -> QOLHuntersMixinPlugin.vaultEnchanterEmeraldSlot && !QOLHuntersMixinPlugin.vanillaSafeMode,
+            "io.iridium.qolhunters.mixin.vaultenchanter.MixinVaultEnchanterContainer", () -> QOLHuntersMixinPlugin.vaultEnchanterEmeraldSlot && !QOLHuntersMixinPlugin.vanillaSafeMode,
+            "io.iridium.qolhunters.mixin.vaultenchanter.MixinVaultEnchanterScreen", () -> QOLHuntersMixinPlugin.vaultEnchanterEmeraldSlot && !QOLHuntersMixinPlugin.vanillaSafeMode,
+            "io.iridium.qolhunters.mixin.vaultenchanter.MixinVaultEnchanterTileEntity", () -> QOLHuntersMixinPlugin.vaultEnchanterEmeraldSlot && !QOLHuntersMixinPlugin.vanillaSafeMode
     );
 
 
-    private static boolean vanillaSafeMode = false;
+
+
+
+
+
+    private static final String CONFIG_FILE_NAME = "qolhunters-client.toml";
+    private static final String VANILLA_SAFE_MODE_CONFIG_VALUE = "General Configs.Vanilla Safe Mode";
+    private static final String BETTER_DESCRIPTIONS_CONFIG_VALUE = "Client-Only Extensions.Better Descriptions";
+    private static final String VAULT_MODIFIER_OVERLAYS_CONFIG_VALUE = "Client-Only Extensions.Vault Modifier Text Overlays";
+    private static final String VAULT_ENCHANTER_EMERALD_SLOT_CONFIG_VALUE = "Client-Server Extensions.Vault Enchanter Emeralds Slot";
+
+
 
     private static void loadConfig(){
         try {
-            Path configPath = FMLPaths.CONFIGDIR.get().resolve("qolhunters-client.toml");
+            Path configPath = FMLPaths.CONFIGDIR.get().resolve(CONFIG_FILE_NAME);
             File configFile = configPath.toFile();
             if (configFile.exists()) {
                 FileConfig config = FileConfig.of(configFile);
                 config.load();
-                vanillaSafeMode = config.getOrElse("Configs for QOLHunters.Vanilla Safe Mode", false);
+                vanillaSafeMode = config.getOrElse(VANILLA_SAFE_MODE_CONFIG_VALUE, false);
+                betterDescriptions = config.getOrElse(BETTER_DESCRIPTIONS_CONFIG_VALUE, true);
+                vaultModifierOverlays = config.getOrElse(VAULT_MODIFIER_OVERLAYS_CONFIG_VALUE, true);
+                vaultEnchanterEmeraldSlot = config.getOrElse(VAULT_ENCHANTER_EMERALD_SLOT_CONFIG_VALUE, true);
+
+                QOLHunters.LOGGER.info("QOLHunters: Vanilla Safe Mode: " + vanillaSafeMode);
+                QOLHunters.LOGGER.info("QOLHunters: Better Descriptions: " + betterDescriptions);
+                QOLHunters.LOGGER.info("QOLHunters: Vault Modifier Text Overlays: " + vaultModifierOverlays);
+                QOLHunters.LOGGER.info("QOLHunters: Vault Enchanter Emeralds Slot: " + vaultEnchanterEmeraldSlot);
+
+                config.close();
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -44,13 +84,24 @@ public final class QOLHuntersMixinPlugin implements IMixinConfigPlugin {
 
     static {
         loadConfig();
-        QOLHunters.LOGGER.info("QOLHUNTERS: Vanilla Safe Mode is {}", vanillaSafeMode);
     }
 
     @Override
     public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
-        boolean shouldApply = CONDITIONS.getOrDefault(mixinClassName, TRUE).get();
-        QOLHunters.LOGGER.info("QOLHUNTERS: Mixin {} should be applied to target {}: {}", mixinClassName, targetClassName, shouldApply);
+
+        boolean shouldApply;
+
+        if(VAULT_ENCHANTER_EMERALD_SLOT_CONDITIONS.containsKey(mixinClassName)){
+            shouldApply = VAULT_ENCHANTER_EMERALD_SLOT_CONDITIONS.getOrDefault(mixinClassName, TRUE).get();
+        } else if(BETTER_DESCRIPTIONS_CONDITIONS.containsKey(mixinClassName)){
+            shouldApply = BETTER_DESCRIPTIONS_CONDITIONS.getOrDefault(mixinClassName, TRUE).get();
+        } else if(VAULT_MODIFIER_OVERLAYS_CONDITIONS.containsKey(mixinClassName)){
+            shouldApply = VAULT_MODIFIER_OVERLAYS_CONDITIONS.getOrDefault(mixinClassName, TRUE).get();
+        } else {
+            shouldApply = TRUE.get();
+        }
+        QOLHunters.LOGGER.info("QOLHunters: shouldApplyMixin: " + mixinClassName + " -> " + shouldApply);
+
         return shouldApply;
     }
 
--- src/main/java/io/iridium/qolhunters/config/QOLHuntersClientConfigs.java
+++ src/main/java/io/iridium/qolhunters/config/QOLHuntersClientConfigs.java
@@ -9,13 +9,38 @@ public class QOLHuntersClientConfigs {
 
     public static final ForgeConfigSpec.ConfigValue<Boolean> VANILLA_SAFE_MODE;
 
+    public static final ForgeConfigSpec.ConfigValue<Boolean> BETTER_DESCRIPTIONS;
+    public static final ForgeConfigSpec.ConfigValue<Boolean> VAULT_MODIFIER_TEXT_OVERLAYS;
+    public static final ForgeConfigSpec.ConfigValue<Boolean> VAULT_ENCHANTER_EMERALDS_SLOT;
+
     static {
-        CLIENT_BUILDER.push("Configs for QOLHunters");
 
-        VANILLA_SAFE_MODE = CLIENT_BUILDER.comment("Disables QOL mods that cannot work on vanilla VH servers, e.g. Emerald Slot in enchantment table\nBe very careful if you change this on single player worlds! For example, if you have emeralds in your enchantment table and you enable this, those will probably get deleted if you load the world before changing this back").define("Vanilla Safe Mode", false);
+        CLIENT_BUILDER.comment("QOLHunters Configuration\nALL CHANGES REQUIRE A CLIENT RESTART");
 
+        CLIENT_BUILDER.push("General Configs");
+
+        VANILLA_SAFE_MODE = CLIENT_BUILDER.comment("Disables QOL mods that cannot work on vanilla VH servers (disables all mods in the 'Client-Server Extensions' list)\nBe very careful if you change this on single player worlds! For example, if you have emeralds in your enchantment table and you enable this, those will probably get deleted if you load the world before changing this back").define("Vanilla Safe Mode", false);
 
         CLIENT_BUILDER.pop();
+
+
+
+
+        CLIENT_BUILDER.push("Client-Only Extensions");
+
+        BETTER_DESCRIPTIONS = CLIENT_BUILDER.comment("Improves the descriptions of abilities, talents, expertises and researches").define("Better Descriptions", true);
+        CLIENT_BUILDER.comment("");
+        VAULT_MODIFIER_TEXT_OVERLAYS = CLIENT_BUILDER.comment("Adds text overlays to the Vault modifiers, e.g. '+10% Damage' or 'Speed +1'").define("Vault Modifier Text Overlays", true);
+
+        CLIENT_BUILDER.pop();
+
+
+
+
+        CLIENT_BUILDER.push("Client-Server Extensions");
+
+        VAULT_ENCHANTER_EMERALDS_SLOT = CLIENT_BUILDER.comment("Adds an emerald slot to the Vault Enchanter").define("Vault Enchanter Emeralds Slot", true);
+
         CLIENT_SPEC = CLIENT_BUILDER.build();
     }
 
