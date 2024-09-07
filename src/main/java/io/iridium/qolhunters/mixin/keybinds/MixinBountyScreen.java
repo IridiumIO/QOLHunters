842393377b4cf1698d81341c5eee5a42b0aca632
IridiumIO <iridium.io@outlook.com>
2024-09-07 14:01:48 +1000

    Fixes critical bug with bounty table rerolling

--- src/main/java/io/iridium/qolhunters/mixin/keybinds/MixinBountyScreen.java
+++ src/main/java/io/iridium/qolhunters/mixin/keybinds/MixinBountyScreen.java
@@ -3,6 +3,7 @@ package io.iridium.qolhunters.mixin.keybinds;
 import com.mojang.blaze3d.platform.InputConstants;
 import io.iridium.qolhunters.util.KeyBindings;
 import iskallia.vault.client.gui.screen.bounty.BountyScreen;
+import iskallia.vault.client.gui.screen.bounty.element.BountyElement;
 import iskallia.vault.client.gui.screen.bounty.element.BountyTableContainerElement;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,27 +12,42 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 @Mixin(BountyScreen.class)
 public abstract class MixinBountyScreen {
 
+
+
     @Shadow(remap = false) @Final
     private BountyTableContainerElement bountyTableContainerElement;
 
     @Inject(method="keyPressed", at=@At("HEAD"), cancellable=true)
-    public void keyPressed(int pKeyCode, int pScanCode, int pModifiers, CallbackInfoReturnable<Boolean> cir) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    public void keyPressed(int pKeyCode, int pScanCode, int pModifiers, CallbackInfoReturnable<Boolean> cir) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, NoSuchFieldException {
         InputConstants.Key key = InputConstants.getKey(pKeyCode, pScanCode);
 
         if (key.equals(KeyBindings.FORGE_ITEM.getKey())) {
 
             //I used reflection. Sue me.
-            Method handleRerollMethod = bountyTableContainerElement.getClass().getDeclaredMethod("handleReroll");
-            handleRerollMethod.setAccessible(true);
-            handleRerollMethod.invoke(bountyTableContainerElement);
 
-            cir.setReturnValue(true);
+            Field bountyElementField = bountyTableContainerElement.getClass().getDeclaredField("bountyElement");
+            bountyElementField.setAccessible(true);
+            BountyElement bountyElement = (BountyElement) bountyElementField.get(bountyTableContainerElement);
+
+            if (bountyElement.getSelectedBounty() == null) {
+                cir.setReturnValue(false);
+            } else if (bountyElement.getStatus() != BountyElement.Status.AVAILABLE) {
+                cir.setReturnValue(false);
+            }else {
+                Method handleRerollMethod = bountyTableContainerElement.getClass().getDeclaredMethod("handleReroll");
+                handleRerollMethod.setAccessible(true);
+                handleRerollMethod.invoke(bountyTableContainerElement);
+
+                cir.setReturnValue(true);
+            }
+
         }
 
     }
