e4d1718d84253484e0c76e8b228f75d8c7a4a509
IridiumIO <iridium.io@outlook.com>
2024-09-10 23:25:46 +1000

    Version 0.5.2 - Forge screens can now be crafted using shift-click or shift-space

--- src/main/java/io/iridium/qolhunters/mixin/keybinds/MixinForgeRecipeContainerScreen.java
+++ src/main/java/io/iridium/qolhunters/mixin/keybinds/MixinForgeRecipeContainerScreen.java
@@ -1,16 +1,31 @@
 package io.iridium.qolhunters.mixin.keybinds;
 
+import io.iridium.qolhunters.mixin.accessors.AccessorAbstractContainerScreen;
 import io.iridium.qolhunters.util.KeyBindings;
 import iskallia.vault.block.entity.base.ForgeRecipeTileEntity;
+import iskallia.vault.client.gui.framework.element.ButtonElement;
+import iskallia.vault.client.gui.framework.render.spi.IElementRenderer;
+import iskallia.vault.client.gui.framework.render.spi.ITooltipRendererFactory;
+import iskallia.vault.client.gui.framework.screen.AbstractElementContainerScreen;
 import iskallia.vault.client.gui.screen.CatalystInfusionTableScreen;
 import iskallia.vault.client.gui.screen.block.InscriptionTableScreen;
 import iskallia.vault.client.gui.screen.block.ToolStationScreen;
 import iskallia.vault.client.gui.screen.block.VaultForgeScreen;
 import iskallia.vault.client.gui.screen.block.base.ForgeRecipeContainerScreen;
 import iskallia.vault.container.spi.ForgeRecipeContainer;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.inventory.ClickType;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.lang.reflect.InvocationTargetException;
@@ -19,8 +34,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @Mixin(ForgeRecipeContainerScreen.class)
-public abstract class MixinForgeRecipeContainerScreen<V extends ForgeRecipeTileEntity, T extends ForgeRecipeContainer<V>>  {
-
+public abstract class MixinForgeRecipeContainerScreen<V extends ForgeRecipeTileEntity, T extends ForgeRecipeContainer<V>> extends AbstractElementContainerScreen<T> {
 
     //Store a list of all screens that are instances of the VaultForgeScreen class
     private static final List<Class> ForgeRecipeContainerScreens = Arrays.asList(
@@ -49,13 +63,37 @@ public abstract class MixinForgeRecipeContainerScreen<V extends ForgeRecipeTileE
 
                 Method onCraftClickMethod = ForgeRecipeContainerScreen.class.getDeclaredMethod("onCraftClick");
                 onCraftClickMethod.setAccessible(true);
-                onCraftClickMethod.invoke((ForgeRecipeContainerScreen<V,T>)(Object)this);
-
-
+                onCraftClickMethod.invoke((ForgeRecipeContainerScreen<V, T>) (Object) this);
                 cir.setReturnValue(true);
             }
         }
 
     }
 
+
+    @Inject(method = "onCraftClick", at = @At(value = "HEAD"), remap = false)
+    public void onCraftClick(CallbackInfo ci) {
+        ForgeRecipeContainer<?> forgeRecipeContainer = ((ForgeRecipeContainerScreen<V, T>) (Object) this).getMenu();
+
+        if (forgeRecipeContainer.getResultSlot().hasItem()) {
+            if (Screen.hasShiftDown()) {
+                ((AccessorAbstractContainerScreen) this).invokeSlotClicked(forgeRecipeContainer.getResultSlot(), forgeRecipeContainer.getResultSlot().index, 0, ClickType.QUICK_MOVE);
+            }
+        }
+    }
+
+
+    @Shadow(remap = false) @Final
+    private ButtonElement<?> craftButton;
+
+    @Inject(method="containerTick", at=@At("TAIL"))
+    protected void qol$containerTick(CallbackInfo ci) {
+        MutableComponent text = new TextComponent("Hold ").withStyle(ChatFormatting.DARK_GRAY).append(new TextComponent("<SHIFT>").withStyle(ChatFormatting.GRAY)).append(" to Quick Move");
+        if(!craftButton.isDisabled()) craftButton.tooltip(() -> text);
+    }
+
+
+    protected MixinForgeRecipeContainerScreen(T container, Inventory inventory, Component title, IElementRenderer elementRenderer, ITooltipRendererFactory<AbstractElementContainerScreen<T>> tooltipRendererFactory) {
+        super(container, inventory, title, elementRenderer, tooltipRendererFactory);
+    }
 }
