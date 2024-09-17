0254562dc6aff52b68578464f5566165b7a966b5
IridiumIO <iridium.io@outlook.com>
2024-09-17 18:31:19 +1000

    Remove dev logging

--- src/main/java/io/iridium/qolhunters/mixin/vaultenchanter/MixinVaultEnchanterBlock.java
+++ src/main/java/io/iridium/qolhunters/mixin/vaultenchanter/MixinVaultEnchanterBlock.java
@@ -26,30 +26,21 @@ public abstract class MixinVaultEnchanterBlock extends FacedBlock implements Ent
      */
     @Overwrite
     public void onRemove(BlockState state, Level level, BlockPos pos, BlockState newState, boolean isMoving) {
-        QOLHunters.LOGGER.info("onRemove called");
         if (!state.is(newState.getBlock()) && level.getBlockEntity(pos) instanceof VaultEnchanterTileEntity enchanter) {
 
             OverSizedInventory ct = ((IModifiedInventory)enchanter).getOverSizedInventory();
-            QOLHunters.LOGGER.info("Dropping oversized contents: " + ct.getContents().stream().count());
             ct.getOverSizedContents().forEach(
                             overSizedStack -> overSizedStack.splitByStackSize()
                                     .forEach(splitStack -> {
-
-                                        QOLHunters.LOGGER.info("Dropping item: " + splitStack + " at " + pos);
                                         Containers.dropItemStack(level, (double)pos.getX(), (double)pos.getY(), (double)pos.getZ(), splitStack);
                                     })
                     );
-
 
             enchanter.getInventory().clearContent();
             ct.clearContent();
             level.updateNeighbourForOutputSignal(pos, this);
         }
 
-        if (IModifiedInventory.class.isAssignableFrom(level.getBlockEntity(pos).getClass())) {
-            QOLHunters.LOGGER.info("Ah fuck");
-
-        }
 
         super.onRemove(state, level, pos, newState, isMoving);
 
