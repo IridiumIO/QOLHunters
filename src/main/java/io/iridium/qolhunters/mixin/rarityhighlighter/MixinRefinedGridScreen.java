c503f043321986583589481af822544115bd2454
IridiumIO <iridium.io@outlook.com>
2024-09-30 22:51:38 +1000

    Adds rarity highlighter to RS grids. Should be added to AE2 but does not work and I don't know why

---
+++ src/main/java/io/iridium/qolhunters/mixin/rarityhighlighter/MixinRefinedGridScreen.java
@@ -0,0 +1,28 @@
+package io.iridium.qolhunters.mixin.rarityhighlighter;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.refinedmods.refinedstorage.screen.BaseScreen;
+import io.iridium.qolhunters.config.QOLHuntersClientConfigs;
+import io.iridium.qolhunters.util.SharedFunctions;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = BaseScreen.class, remap = false)
+public class MixinRefinedGridScreen {
+
+
+    @Inject(method="renderItem(Lcom/mojang/blaze3d/vertex/PoseStack;IILnet/minecraft/world/item/ItemStack;ZLjava/lang/String;I)V", at=@At("HEAD"))
+    private void renderItem(PoseStack poseStack, int x, int y, ItemStack stack, boolean overlay, String text, int textColor, CallbackInfo ci) {
+        if (!QOLHuntersClientConfigs.RARITY_HIGHLIGHTER.get() ) return;
+
+        if((InputConstants.isKeyDown(Minecraft.getInstance().getWindow().getWindow(), InputConstants.KEY_LSHIFT))){
+            SharedFunctions.renderSlotRarityHighlight(poseStack, stack, x, y);
+        }
+    }
+
+}
