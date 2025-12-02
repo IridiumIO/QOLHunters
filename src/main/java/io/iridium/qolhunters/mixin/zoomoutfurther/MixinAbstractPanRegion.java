b018723186f71a1c620582f00614cfaf79cbc03d
radimous <jurda92@gmail.com>
2025-12-02 16:46:16 +0100

    add further zoom out to vault map and make it configurable

---
+++ src/main/java/io/iridium/qolhunters/mixin/zoomoutfurther/MixinAbstractPanRegion.java
@@ -0,0 +1,21 @@
+package io.iridium.qolhunters.mixin.zoomoutfurther;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import io.iridium.qolhunters.config.QOLHuntersClientConfigs;
+import iskallia.vault.client.gui.screen.player.legacy.tab.split.spi.AbstractPanRegion;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(value = AbstractPanRegion.class, remap = false)
+public class MixinAbstractPanRegion {
+
+    @WrapOperation(method = "clampViewportScale", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Mth;clamp(FFF)F"))
+    private float zoomOutFurther(float pValue, float pMin, float pMax, Operation<Float> original) {
+        // Mth.clamp(this.viewportScale, 0.3F, 5F)
+        if (QOLHuntersClientConfigs.ZOOM_OUT_FURTHER.get()) {
+            return original.call(pValue, 0.1F, pMax);
+        }
+        return original.call(pValue, pMin, pMax);
+    }
+}
